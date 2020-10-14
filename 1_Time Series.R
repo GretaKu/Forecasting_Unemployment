8218e72b3506bfe4d35c123562d68379c9fbd7c6
GretaKurpicz <greta.kurpicz(at)gmail.com>
2020-10-14 17:16:39 +0200

    Code for trend and season

--- 1_Time Series.R
+++ 1_Time Series.R
@@ -29,3 +29,27 @@ addLegend("topleft", on=1,
           lty=c(1, 1),
           col=c("black", "red"))
 
+
+
+## Deterministic trend
+model_Trend <- lm(log(data$arbeitslose) ~ c(1:length(data$arbeitslose)))
+plot.xts(cbind(log(data$arbeitslose),model_Trend$fitted.values), main = "Unemployment and linear trend")
+
+#plot residuals
+plot.xts(as.xts(model_Trend$residuals),  main = "Unemployment: deviations from linear trend")
+
+## Stochastic Trend
+dlnData = diff.xts(as.xts(log(data)),1,1,na.pad=FALSE,log=TRUE)
+plot.xts(dlnData$arbeitslose,  main = "Unemployment: quarterly growth, non-seasonally adjusted")
+
+
+## Deterministic season
+quarterlyDummies <- forecast::seasonaldummy(ts_ts(data$arbeitslose))
+model_Season <- lm(log(data$arbeitslose) ~ quarterlyDummies)
+
+#plot
+plot.xts(as.xts(model_Trend$residuals),  main = "Unemployment: deviations from linear trend and seasonal dummy")
+
+
+
+
